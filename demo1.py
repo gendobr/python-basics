8202c73c807d39a21e8c5b032895c7810aa23b8e
Hennadii Dobrovolskyi <gen.dobr@gmail.com>
2023-02-16 17:32:09 +0200

    first commit

---
+++ demo1.py
@@ -0,0 +1,6 @@
+prompt = "Надрукуйте ім'я:"
+numB = 234
+numA = 4.6
+a754iuio_o = input("Надрукуйте ім'я:")
+print("привіт")
+print(a754iuio_o)
---
+++ demo2.py
@@ -0,0 +1,5 @@
+prompt = "Надрукуйте число:"
+numA = int(input(prompt))
+numB = int(input(prompt))
+print("Сума")
+print(numA+numB)
