fcf3f72d6c4581fab1485eb8b74b07b5a563b348
Hennadii Dobrovolskyi <gen.dobr@gmail.com>
2023-03-07 17:22:50 +0200

    Classes

---
+++ demo16.py
@@ -0,0 +1,33 @@
+class Duck:
+    size: float  # атрибут
+    wings: List[wing]  # атрибут
+    legs: List[legs]  # атрибут
+    head: Head  # атрибут
+    beak: Beak  # атрибут
+
+    # конструктор
+    def __init__(self) -> None:
+        self.size = 10
+        self.wings = [Wing(left, 30, 100), Wing(right, 30, 100)]
+        self.head = Head()
+        self.beak = Beak()
+
+    # метод
+    def sing(self):
+        """"""
+
+    # метод
+    def eat(self, food):
+        """"""
+
+    # метод
+    def drink(self, food):
+        """"""
+
+
+BasilDuck = Duck()
+
+BasilDuck.sing()
+BasilDuck.eat("Bucket of beans")
+
+print(BasilDuck.size)
